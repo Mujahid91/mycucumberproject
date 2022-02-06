b98c84b4c675df1f45aa9c04d758c8540e43e5ed
Mujohid Kahhori <mujohidkahhori@Mujohids-MacBook-Pro.local>
2022-02-06 06:56:11 +0300

    first commit

---
+++ src/test/java/Hooks/Hooks.java
@@ -0,0 +1,24 @@
+package Hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.Driver;
+
+public class Hooks {
+    @Before
+    public void setUp(){
+        //Runs before each scenario
+        System.out.println("Before Hooks");
+    }
+    @After
+    public void tearDown(Scenario scenario){
+        final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()) {
+            scenario.attach(screenshot, "image/png","screenshots");
+        }
+        Driver.closeDriver();
+    }
+}
